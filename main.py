6cd14c3dc4c60cf75b73f22f05f9273a97a0bc0f
frizzo69 <137474876+frizzo69@users.noreply.github.com>
2026-03-28 09:27:17 +0530

    dotenv token fetch

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import discord
 import os
 import asyncio
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True 
